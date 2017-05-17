195b490e12040fb6a9302e6e4820ee5db0c08051
Riju Ghosh <riju.open@gmail.com>
2017-05-17 12:40:05 -0700

    Queue Using Lists
    Implemented Queue Using Lists

---
+++ linkedList/C/queue_using_lists.c
@@ -0,0 +1,92 @@
+/*
+ *	queue_using_lists.c
+ *
+ *	Here, a Queue has been implemented using Linked List, and enqueue and dequeue functions.
+ *	read: https://en.wikipedia.org/wiki/Queue_(abstract_data_type)
+ *
+ *	Written by- Riju Ghosh | Ultimate_Learner | www.rijughosh.com | mail@rijughosh.com
+ */
+
+ #include"linked_list_def.h"
+
+
+ /*
+  *	This is a simple structure of a queue, containing a rear pointer.
+  */
+ struct Queue{struct Node*rear;};
+
+
+
+ /*
+  *	This function creates a new Queue, and returns the queue pointer.
+  */
+ struct Queue* create_queue()
+ {
+ 	struct Queue*queue=(struct Queue*)malloc(sizeof(struct Queue));
+ 	queue->rear=NULL;
+ 	return queue;
+ }
+
+
+
+ /*
+  *	We enqueue(or insert) the new element in the rear of the queue, or back of the list
+  *   here front is there just for logical understanding, it is not implemented in that way.
+  *	eg. if existing list is:
+  *					rear -> 1 -> 2 -> 3 <- front
+  *	we want to enqueue 100 then:
+  *					rear -> 100 -> 1 -> 2 -> 3 <- front
+  */
+ void enqueue(struct Queue*queue,int data)
+ {
+ 	insert_first(&(queue->rear),data);
+ }
+
+
+
+ /*
+  *	We remove the front element, or the first entered element.
+  *	eg. if the existing list is:
+  *						rear -> 1 -> 2 -> 3 <- front
+  *	on executing dequeue, it returns 3 & the list becomes
+  *						rear -> 1 -> 2 <- front
+  */
+ struct Node*dequeue(struct Queue*queue)
+ {
+ 	assert(!is_list_empty(queue->rear));
+ 	return rem_end(&(queue->rear));
+ }
+
+
+
+ /*
+  *	This returns whether the Queue is empty
+  */
+ int is_queue_empty(struct Queue*queue)
+ {
+ 	return is_list_empty(queue->rear); // 1 if Empty; 0 if NOT Empty.
+ }
+
+
+
+ /*
+  *	Tests the Queue functionalities
+  */
+ int main()
+ {
+ 	struct Queue*queue=create_queue();
+ 	/*
+ 	 * Insert your own data...
+ 	 */
+    enqueue(queue,10);
+    enqueue(queue,20);
+    enqueue(queue,30);
+    enqueue(queue,40);
+    enqueue(queue,50);
+ 	/*
+ 	 */
+ 	while(is_queue_empty(queue)!=1)
+ 			printf("%d\n",dequeue(queue)->data);
+
+ 	return 0;
+ }
--- linkedList/C/stack_using_lists.c
+++ linkedList/C/stack_using_lists.c
@@ -1,44 +1,91 @@
 /*
- *      stack_using_lists.c
+ *	stack_using_lists.c
  *
- *      Here, a Stack has been implemented using Linked List.
+ *	Here, a Stack has been implemented using Linked List, and push and pop functions.
+ *	read: https://en.wikipedia.org/wiki/Stack_(abstract_data_type)
  *
- *      Written by- Riju Ghosh | Ultimate_Learner | www.rijughosh.com | mail@rijughosh.com
+ *	Written by- Riju Ghosh | Ultimate_Learner | www.rijughosh.com | mail@rijughosh.com
  */
 
 #include"linked_list_def.h"
 
-void push(struct Node**top,int data)
+
+/*
+ *	This is a simple structure of a stack, containing only a Top pointer.
+ */
+struct Stack{struct Node*top;};
+
+
+
+/*
+ *	This function creates a new Stack, and returns the stack pointer.
+ */
+struct Stack* create_stack()
 {
-	insert_first(top,data);
+	struct Stack*stack=(struct Stack*)malloc(sizeof(struct Stack));
+	stack->top=NULL;
+	return stack;
 }
 
-struct Node*pop(struct Node**top)
+
+
+/*
+ *	We push the new element in the front of the list, or TOP of the list
+ *	eg. if existing list is:
+ *					top -> 1 -> 2 -> 3 -> NULL
+ *	we want to push 100 then:
+ *					top -> 100 -> 1 -> 2 -> 3 -> NULL
+ */
+void push(struct Stack*stack,int data)
 {
-	assert(!is_list_empty(*top));
-	return rem_first(top);
+	insert_first(&(stack->top),data);
 }
 
-int is_stack_empty(struct NOde*top)
+
+
+/*
+ *	We remove the top element, or the last entered element.
+ *	eg. if the existing list is:
+ *						top -> 1 -> 2 -> 3 -> NULL
+ *	on executing pop, it returns 1 & the list becomes
+ *						top -> 2 -> 3 -> NULL
+ */
+struct Node*pop(struct Stack*stack)
 {
-	return is_list_empty(top); // 1 if Empty; 0 if NOT Empty.
+	assert(!is_list_empty(stack->top));
+	return rem_first(&(stack->top));
 }
 
+
+
+/*
+ *	This returns whether the Stack is empty
+ */
+int is_stack_empty(struct Stack*stack)
+{
+	return is_list_empty(stack->top); // 1 if Empty; 0 if NOT Empty.
+}
+
+
+
+/*
+ *	Tests the Stack functionalities
+ */
 int main()
 {
-	struct Node*top=NULL;
+	struct Stack*stack=create_stack();
 	/*
 	 * Insert your own data...
 	 */
-	push(&top,5);
-	push(&top,4);
-	push(&top,3);
-	push(&top,2);
-	push(&top,1);
+	push(stack,5);
+	push(stack,4);
+	push(stack,3);
+	push(stack,2);
+	push(stack,1);
 	/*
 	 */
-	while(is_stack_empty(top)!=1)
-			printf("%d\n",pop(&top)->data);
+	while(is_stack_empty(stack)!=1)
+			printf("%d\n",pop(stack)->data);
 
 	return 0;
 }
